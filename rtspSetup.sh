2f160b1a251731334f7cac3fa40e23789b18f443
unknown <eeshvij@gmail.com>
2023-02-07 17:24:40 -0800

    added in loopback enabling virtual camera output

--- rtspSetup.sh
+++ rtspSetup.sh
@@ -7,8 +7,11 @@ mkdir ~/rtsp
 cd ~/rtsp
 
 sudo apt-get install libgstrtspserver-1.0 libgstreamer1.0-dev -y
+sudo apt-get -y install v4l-utils -y
 sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio -y
-
+sudo pip3 install numpy
+sudo apt-get install v4l2loopback-utils -y
+sudo pip3 install pyfakewebcam
 
 git clone https://github.com/Dat-Bois/gst-rtsp-server.git
 
@@ -19,6 +22,7 @@ gcc test-launch.c -o test-launch $(pkg-config --cflags --libs gstreamer-1.0 gstr
 cd ../.. 
 
 touch start_video.sh
+touch test.py
 
 PRODUCT=$(sudo lshw -json | jq '.product') || PRODUCT=$(sudo lshw -json | jq '.[].product')
 
@@ -44,5 +48,7 @@ else
 fi
 ' >> start_video.sh
 fi
+
+cp ~/installScripts/test.py ~/rtsp/
 
 sudo chmod +x start_video.sh
---
+++ test.py
@@ -0,0 +1,37 @@
+import cv2
+import time
+import pyfakewebcam
+import numpy as np
+import os
+import sys
+
+
+os.system("sudo modprobe v4l2loopback devices=2")  # will create two fake webcam devices
+
+IMG_W = 1280
+IMG_H = 720
+
+cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, IMG_W)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, IMG_H)
+
+fake1 = pyfakewebcam.FakeWebcam('/dev/video8', IMG_W, IMG_H)
+fake2 = pyfakewebcam.FakeWebcam('/dev/video9', IMG_W, IMG_H)
+
+while True:
+    try:    
+        ret, frame = cam.read()
+
+        flipped = cv2.flip(frame, 1)
+
+        # Mirror effect 
+        frame[0 : IMG_H, IMG_W//2 : IMG_W] = flipped[0 : IMG_H, IMG_W//2 : IMG_W]
+
+        fake1.schedule_frame(frame)
+        fake2.schedule_frame(flipped)
+
+        time.sleep(1/15.0)
+    except KeyboardInterrupt:
+        print("Exiting...")
+        os.system("sudo modprobe -r v4l2loopback")
+        sys.exit()
