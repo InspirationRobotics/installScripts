9c2fa3b371e6b00266d1235dbf8660362c526a7b
Eesh <pc@MacBookPro-3.local>
2023-02-09 09:53:02 -0800

    bug fixes

--- runFirst.sh
+++ runFirst.sh
@@ -4,10 +4,10 @@ echo "Thanks for following directions and running this script first, you're not 
 sleep 1
 for (( ; ; ))
 do
-    echo "Are you running as sudo? (Like sudo ./runFirst.sh not in su or anything)"
+    echo "Are you running as sudo? (Like sudo ./runFirst.sh not in su or anything) (y/n)"
     read question
 
-    if [ "$question" = "yes" || "$question" = "y" ]; then
+    if [ "$question" = "y" ]; then
         echo "Whoo lets go your not dumb"
         sleep 1
         echo "Initializing stuff..."
@@ -16,11 +16,16 @@ do
         sudo chmod +x test.sh
         echo "See that wasn't bad :), now run this script as sudo in the terminal you are installing stuff in at least once everytime so stuff doesn't go poop"
 	break
-    elif [ "$question" = "no" || "$question" = "n" ]; then
+    elif [ "$question" = "n" ]; then
         echo "You absolute buffoon, run as sudo and then i'll do some fancy shmancy stuff"
 	break
+    elif [ "$question" = "\"y\"" ]; then
+        echo "I hate you"
+	break
     else
-        echo "wowwwwww you thought you were cool and quirky by not following the norm, well guess what, you're not. \n Run with sudo and type yes like a normal person"
+        echo "wowwwwww you thought you were cool and quirky by not following the norm, well guess what, you're not."
+        echo "Run with sudo and type "y" like a normal person"
+        break
     fi
 done
 
