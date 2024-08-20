9e87216ebf5d24fe0b638a1e2dab9bf1f9d619d3
Dat-Bois <eeshvij@gmail.com>
2024-08-19 19:25:06 -0700

    fixed mlSetup and also updated nomachine

--- nomachineSetup.sh
+++ nomachineSetup.sh
@@ -1,4 +1,4 @@
 cd ~/Downloads
-wget https://download.nomachine.com/download/8.11/Arm/nomachine_8.11.3_3_arm64.deb
-sudo dpkg -i nomachine_8.11.3_3_arm64.deb
+wget https://download.nomachine.com/download/8.13/Arm/nomachine_8.13.1_1_arm64.deb
+sudo sudo dpkg -i nomachine_8.13.1_1_arm64.deb
 echo "System needs reboot before NoMachine can be used..."
